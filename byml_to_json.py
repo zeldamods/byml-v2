84308ac7d6a6b5c7c96435ff69371674d168094b
Léo Lam <leo@innovatetechnologi.es>
2018-04-26 09:49:47 +0200

    Add byml_to_json.py

---
+++ byml_to_json.py
@@ -0,0 +1,12 @@
+import json
+import sys
+
+import byml
+
+if len(sys.argv) != 2 or sys.argv[1] in ['-h', '--help']:
+    sys.stderr.write("Usage: byml_to_json.py <BYML>")
+
+with open(sys.argv[1], "rb") as file:
+    data = file.read()
+    root = byml.Byml(data).parse()
+    json.dump(root, sys.stdout, indent=2)
