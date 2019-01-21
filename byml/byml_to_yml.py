21db57cb1c54b6109de6cc5f3ea30522977edc8e
Léo Lam <leo@innovatetechnologi.es>
2019-01-21 21:50:55 +0100

    byml_to_yml: Add option to dump to JSON
    Useful for web things.

--- byml/byml_to_yml.py
+++ byml/byml_to_yml.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import os
 import sys
 import yaml
@@ -9,6 +10,7 @@ from . import yaml_util
 
 def main() -> None:
     parser = argparse.ArgumentParser(description='Converts a BYML file to YAML.')
+    parser.add_argument('-j', '--to-json', action='store_true', help='Convert to JSON (warning: one-way conversion; does not preserve type information)')
     parser.add_argument('byml', help='Path to a BYML file', nargs='?', default='-')
     parser.add_argument('yml', help='Path to destination YAML file', nargs='?', default='-')
     args = parser.parse_args()
@@ -30,7 +32,10 @@ def main() -> None:
             sys.exit(1)
         output = sys.stdout if args.yml == '-' else open(args.yml, 'w', encoding='utf-8')
         with output:
-            yaml.dump(root, output, Dumper=dumper, allow_unicode=True, encoding='utf-8')
+            if args.to_json:
+                json.dump(root, output, ensure_ascii=False)
+            else:
+                yaml.dump(root, output, Dumper=dumper, allow_unicode=True, encoding='utf-8')
 
 if __name__ == '__main__':
     main()
