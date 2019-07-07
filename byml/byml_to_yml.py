a94a82666661638cb55f8b774ea611edd55892e3
Léo Lam <leo@leolam.fr>
2019-07-07 22:17:50 +0200

    Update PyYAML dependency

--- byml/byml_to_yml.py
+++ byml/byml_to_yml.py
@@ -35,7 +35,7 @@ def main() -> None:
             if args.to_json:
                 json.dump(root, output, ensure_ascii=False)
             else:
-                yaml.dump(root, output, Dumper=dumper, allow_unicode=True, encoding='utf-8')
+                yaml.dump(root, output, Dumper=dumper, allow_unicode=True, encoding='utf-8', default_flow_style=None)
 
 if __name__ == '__main__':
     main()
